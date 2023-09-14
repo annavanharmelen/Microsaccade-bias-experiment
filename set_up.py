3cf9d22db8f68f43aa3a9df764e1681c866dec50
Anna work laptop <a.m.van.harmelen@vu.nl>
2023-09-14 16:09:13 +0200

    update path

--- set_up.py
+++ set_up.py
@@ -21,7 +21,7 @@ def get_monitor_and_dir(testing: bool):
             "distance": 50,  # in cm
         }
 
-        directory = r"C:\Users\annav\Documents\Jottacloud\Neuroscience PhD\Experiments\microsaccade bias experiments\Data\test"
+        directory = r"C:\Users\annav\Documents\Jottacloud\Neuroscience PhD\Experiments\Microsaccade bias experiments\Data\test"
 
     else:
         # lab
