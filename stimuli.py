fe7f38c9baea0136de1b1a0983ac3d63cf281d90
Anna work laptop <a.m.van.harmelen@vu.nl>
2023-10-03 14:20:05 +0200

    change stimuli sizes and way they are calculated

--- stimuli.py
+++ stimuli.py
@@ -8,16 +8,16 @@ made by Anna van Harmelen, 2023
 
 from psychopy import visual
 from numpy import zeros
+from math import sqrt
 
-ECCENTRICITY = 3
-VERTICAL_POSITION = -3
-GABOR_SIZE = 2  # diameter of Gabor
-CAPTURE_CUE_SIZE = 0.7  # diameter of circle
+ECCENTRICITY = 4
+GABOR_SIZE = 1  # diameter of Gabor
+DOT_SIZE = 0.05  # diameter of circle
 
 
-def create_fixation_cross(settings, colour="#eaeaea"):
+def create_fixation_dot(settings, colour="#eaeaea"):
     # Determine size of fixation cross
-    fixation_size = settings["deg2pix"](0.1)
+    fixation_size = settings["deg2pix"](DOT_SIZE)
 
     # Make fixation dot
     fixation_dot = visual.Circle(
@@ -35,13 +35,13 @@ def make_one_gabor(orientation, colour, position, settings):
     # Check input
     if position == "left":
         pos = (
-            -settings["deg2pix"](ECCENTRICITY),
-            settings["deg2pix"](VERTICAL_POSITION),
+            -settings["deg2pix"](sqrt(1 / 2 * ECCENTRICITY**2)),
+            -settings["deg2pix"](sqrt(1 / 2 * ECCENTRICITY**2)),
         )
     elif position == "right":
         pos = (
-            settings["deg2pix"](ECCENTRICITY),
-            settings["deg2pix"](VERTICAL_POSITION),
+            settings["deg2pix"](sqrt(1 / 2 * ECCENTRICITY**2)),
+            -settings["deg2pix"](sqrt(1 / 2 * ECCENTRICITY**2)),
         )
     elif position == "middle":
         pos = (0, 0)
@@ -49,21 +49,19 @@ def make_one_gabor(orientation, colour, position, settings):
         raise Exception(f"Expected 'left' or 'right', but received {position!r}. :(")
 
     # Create texture for Gabor stimulus
-    gabor_texture = zeros(
-        [256, 256, 4], "f"
-    )
+    gabor_texture = zeros([128, 128, 4], "f")
     gabor_texture[:, :, 0] = colour[0]
     gabor_texture[:, :, 1] = colour[1]
     gabor_texture[:, :, 2] = colour[2]
     gabor_texture[:, :, 3] = -visual.filters.makeGrating(
-        256, gratType="sin", cycles=10.5, ori=orientation
+        128, gratType="sin", cycles=10.5, ori=orientation
     )
 
     # Create Gabor grating stimulus
     gabor_stimulus = visual.GratingStim(
         win=settings["window"],
         units="pix",
-        size=(256, 256),
+        size=(128, 128),
         pos=pos,
         tex=gabor_texture,
         mask="raisedCos",
@@ -73,20 +71,9 @@ def make_one_gabor(orientation, colour, position, settings):
     return gabor_stimulus
 
 
-def create_stimuli_frame(left_orientation, right_orientation, stim_colours, settings, fix_colour="#eaeaea"):
-    create_fixation_cross(settings, fix_colour)
+def create_stimuli_frame(
+    left_orientation, right_orientation, stim_colours, settings, fix_colour="#eaeaea"
+):
+    create_fixation_dot(settings, fix_colour)
     make_one_gabor(left_orientation, stim_colours[0], "left", settings).draw()
     make_one_gabor(right_orientation, stim_colours[1], "right", settings).draw()
-
-
-def create_cue_frame(colour, settings):
-    capture_cue = visual.Circle(
-        win=settings["window"],
-        units="pix",
-        radius=settings["deg2pix"](CAPTURE_CUE_SIZE / 2),
-        pos=(0, 0),
-        fillColor=colour,
-    )
-
-    capture_cue.draw()
-    create_fixation_cross(settings)
